1dddbd080b25fcfc0e9ff2ff1c14e646fc127cb7
Sargam Thakur <sargamthakur665@gmail.com>
2025-04-22 19:40:18 -0400

    more changes

--- rabin_karp.py
+++ rabin_karp.py
@@ -11,19 +11,16 @@ def rabin_karp(pattern, text):
     start_time = time.process_time()
     num = 0
     hpattern = hash_function(pattern)
-    # print("hpattern: ", hpattern)
     h_old = hash_function(text[0:(len(pattern))]) # length of ascii hash   
     if h_old == hpattern and pattern == text[i:i+3]:
         num += 1
     for i in range(1, len(text)-len(pattern)+1):
         h_new = ((256 * (h_old - (ord(text[i-1]) * ((256) ** (len(pattern)-1)))))  + ord(text[i+len(pattern)-1])) % 101
-        # print("i: ",i, "    hnew: ", h_new)
         h_old = h_new
         if h_new == hpattern and pattern == text[i:i+3]:
             num += 1
     end_time = time.process_time()
     cpu_time = end_time-start_time
-    # print("CPU Time:", cpu_time, "seconds")
     return num, cpu_time
 
 # print("Occurrences:", rabin_karp("XYZ", "cjoisdCCBiodjCCB"))
@@ -70,3 +67,27 @@ tracemalloc.start()
 rabin_karp("12345678901234567890123456789012345678901234567890", s1)
 print(tracemalloc.get_traced_memory())
 tracemalloc.stop()
+
+
+
+# algorithm without cpu time and memory usage measurements
+
+# def hash_function(pattern):
+#     h = 0
+#     for i in range(len(pattern)):
+#         h += ord(pattern[i]) * ((256) ** (len(pattern)-i-1)) 
+#     h = h % 101
+#     return h
+
+# def rabin_karp(pattern, text):
+#     count = 0
+#     hpattern = hash_function(pattern)
+#     h_old = hash_function(text[0:(len(pattern))]) 
+#     if h_old == hpattern and pattern == text[i:i+3]:
+#         count += 1
+#     for i in range(1, len(text)-len(pattern)+1):
+#         h_new = ((256 * (h_old - (ord(text[i-1]) * ((256) ** (len(pattern)-1)))))  + ord(text[i+len(pattern)-1])) % 101
+#         h_old = h_new
+#         if h_new == hpattern and pattern == text[i:i+3]:
+#             count += 1
+#     return count
